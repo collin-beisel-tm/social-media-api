7e829385be67325aaee0b9261b65bcae61204405
cbeisel <collin.beisel@toyota.com>
2022-02-26 10:58:46 -0600

    sudo coded controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,30 @@
+const {thought} = require('../models');
+
+const thoughtController = {
+//   /api/thoughts
+
+    //Get all thoughts
+
+
+    //GET thought by ID
+
+
+    // POST new thought and push new id to associated Users thoughts array... thoughtText, username, userId
+
+
+    //PUT update a thought by ID
+
+
+    //DELETE thought by ID
+
+
+//  /api/thoughts/:thoughtId/reactions
+
+
+    //POST new reaction to thoughts reactions array
+
+
+    //DELETE to pull and remove reaction by ID from thoughts reactions array
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,30 @@
+const {user} = require('../models');
+
+const userController = {
+//  /api/users routes
+
+    //GET all users
+
+
+    //GET single user by ID and populated thought/friends data
+
+
+    //POST new user... username/email
+
+
+    //PUT update user by ID
+
+
+    //DELETE user by ID.... bonus delete associated thoughts too
+
+
+//  /api/users/:userId/friends/:friendId
+
+    // POST a new friend to a users friend list
+
+    
+    // DELETE a friend form a users friend list
+
+};
+
+module.exports = userController;
